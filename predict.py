ebc7964a50e63efed8894fdaa59473a8fb239491
dang <nguyenhaidang.uit@gmail.com>
2020-11-08 00:35:34 +0100

    add missing param

--- predict.py
+++ predict.py
@@ -8,7 +8,7 @@ def run():
     model = load_model(model_name)
     print("Predict")
     img_path = os.path.join(DATA_DIR, 'A1', 'plant001_rgb.png')
-    result = model.predict()
+    result = model.predict(img_path)
     print(result)
 
 if __name__ == '__main__':
