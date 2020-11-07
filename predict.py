e64b36a73f34be538c3ddd8682f57f7695c67f10
dang <nguyenhaidang.uit@gmail.com>
2020-11-07 23:25:27 +0100

    use os to join path

--- predict.py
+++ predict.py
@@ -1,10 +1,10 @@
+import os
 from tensorflow.keras.models import load_model
 from configs.global_vars import MODEL_DIR
 
-model_name = MODEL_DIR + 'model_2020-10-31_22-59-26'
-
 def run():
     print("Load model")
+    model_name = os.path.join(MODEL_DIR, 'model_2020-10-31_22-59-26')
     model = load_model(model_name)
     print("Evaluate")
     result = model.evaluate()
