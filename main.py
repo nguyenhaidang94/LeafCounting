f21abd022a15f148ac8ff81a0efffeed4cbf9751
dang <nguyenhaidang.uit@gmail.com>
2020-11-08 21:00:41 +0100

    send notification when finish

--- main.py
+++ main.py
@@ -1,24 +1,31 @@
+import traceback
 from configs.global_vars import DATA_DIR, SUB_DIRS, IMAGE_SIZE, TRAIN_RATIO, VAL_RATIO\
     , OPTIMIZER, BATCH_SIZE, N_EPOCHS
 from model.resnet import Resnet
+from utils.mail_utils import send_email
 
 def run():
-    model = Resnet()
-    print("Load data")
-    model.load_data(DATA_DIR, SUB_DIRS, IMAGE_SIZE, TRAIN_RATIO, VAL_RATIO)
-    print("Build model")
-    model.build()
-    print("Summary")
-    model.summary()
-    print("Compile model")
-    model.compile(optimizer=OPTIMIZER)
-    print("Train model")
-    model.train(BATCH_SIZE, N_EPOCHS)
-    print("Evaluate on test set")
-    result = model.evaluate()
-    print("Loss: {}".format(result))
-    print("Save model")
-    model.save()
+    try:
+        model = Resnet()
+        print("Load data")
+        model.load_data(DATA_DIR, SUB_DIRS, IMAGE_SIZE, TRAIN_RATIO, VAL_RATIO)
+        print("Build model")
+        model.build()
+        print("Summary")
+        model.summary()
+        print("Compile model")
+        model.compile(optimizer=OPTIMIZER)
+        print("Train model")
+        model.train(BATCH_SIZE, N_EPOCHS)
+        print("Evaluate on test set")
+        result = model.evaluate()
+        print("Loss: {}".format(result))
+        print("Save model")
+        model.save()
+        send_email("Prim info", "Training has finished!")
+    except Exception as e:
+        traceback.print_exc()
+        send_email("Prim error", str(e))
 
 if __name__ == '__main__':
     run()
--- predict.py
+++ predict.py
@@ -1,21 +1,28 @@
 import os
 import numpy as np
 from tensorflow.keras.models import load_model
+import traceback
 from configs.global_vars import MODEL_DIR, DATA_DIR, IMAGE_SIZE
 from dataloader.dataloader import DataLoader
+from utils.mail_utils import send_email
 
 def run():
-    print("Load model")
-    model_name = os.path.join(MODEL_DIR, 'model_2020-11-08_00-48-42')
-    model = load_model(model_name)
+    try:
+        print("Load model")
+        model_name = os.path.join(MODEL_DIR, 'model_2020-11-08_00-48-42')
+        model = load_model(model_name)
 
-    print("Predict")
-    img_path = os.path.join(DATA_DIR, 'A1', 'plant001_rgb.png')
-    (w,h,c) = IMAGE_SIZE
-    x = DataLoader().load_rgb_img(img_path, target_size=(w,h))
-    x = np.reshape(x, (1, w, h, c))
-    result = model.predict(x)
-    print("Number of leaves: {}".format(int(result[0][0])))
+        print("Predict")
+        img_path = os.path.join(DATA_DIR, 'A1', 'plant001_rgb.png')
+        (w,h,c) = IMAGE_SIZE
+        x = DataLoader().load_rgb_img(img_path, target_size=(w,h))
+        x = np.reshape(x, (1, w, h, c))
+        result = model.predict(x)
+        print("Number of leaves: {}".format(int(result[0][0])))
+        send_email("Prim", "Prediction has finished!")
+    except Exception as e:
+        traceback.print_exc()
+        send_email("Prim error", str(e))
 
 if __name__ == '__main__':
     run()
