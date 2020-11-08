d79823608b7c8362a4e91b2c50b7c82806bba1a9
dang <nguyenhaidang.uit@gmail.com>
2020-11-08 12:33:03 +0100

    update prediction

--- predict.py
+++ predict.py
@@ -1,15 +1,21 @@
 import os
+import numpy as np
 from tensorflow.keras.models import load_model
-from configs.global_vars import MODEL_DIR, DATA_DIR
+from configs.global_vars import MODEL_DIR, DATA_DIR, IMAGE_SIZE
 
 def run():
     print("Load model")
     model_name = os.path.join(MODEL_DIR, 'model_2020-11-08_00-48-42')
     model = load_model(model_name)
+
     print("Predict")
     img_path = os.path.join(DATA_DIR, 'A1', 'plant001_rgb.png')
-    result = model.predict(img_path)
+    (w,h,c) = IMAGE_SIZE
+    x = DataLoader().load_rgb_img(img_path, target_size=(w,h))
+    x = np.reshape(x, (1, w, h, c))
+    result = model.predict(x)
     print(result)
+    print(int(result[0][0]))
 
 if __name__ == '__main__':
     run()
