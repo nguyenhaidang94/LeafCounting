f3c99ed6789b3ddaeb68bb5d30b48a3006095b02
dang <nguyenhaidang.uit@gmail.com>
2020-11-20 17:55:23 +0100

    calculate mse, mae without lib

--- utils/eval_utils.py
+++ utils/eval_utils.py
@@ -1,11 +1,10 @@
 import numpy as np
-from sklearn.metrics import mean_squared_error, mean_absolute_error
 
 def mse(y_true, y_predict):
-    return mean_squared_error(y_true, y_predict)
+    return np.mean(np.square(y_true - y_predict))
 
 def mae(y_true, y_predict):
-    return mean_absolute_error(y_true, y_predict)
+    return np.mean(np.abs(y_true - y_predict))
 
 def acc(y_true, y_predict):
     return np.sum(y_true == y_predict)/len(y_true)
